d6a7852e38f2535bb5dede6600c5f760e028e129
l-papadopol <117158814+l-papadopol@users.noreply.github.com>
2022-12-10 23:50:43 +0100

    Add files via upload

--- congetture_beal_collatz_cramer.c
+++ congetture_beal_collatz_cramer.c
@@ -19,6 +19,9 @@
 
 int verifica_collatz(void);
 int verifica_primi(unsigned long int);
+int fattori_primi(unsigned long int,
+                  unsigned long int,
+                  unsigned long int);
 
 /******************************/
 /* Definizione delle funzioni */
@@ -31,10 +34,14 @@ int main(void)
   int congettura; /* input: selezione congettura */
 
   /* visualizzazione interfaccia menù di scelta */
-  printf("Quale congettura vuoi considerare?\n"
-         "-Digita 1 per la congettura di Beal\n"
-         "-Digita 2 per la congettura di Collatz\n"
-         "-Digita 3 per la congettura di Cramer\n");
+  printf(" PROGRAMMA DI TEST CONGETTURE DI BEAL - COLLATZ - CRAMÈR \n"
+         "---------------------------------------------------------\n"
+         "[1] Beal\n"
+         "[2] Collatz\n"
+         "[3] Cramèr\n"
+         "----------------------------------------------------------\n"
+         "Digita il numero relativo alla tua scelta e premi invio. \n"
+         );
 
   /* acquisizione scelta */
   scanf("%d",
@@ -52,20 +59,21 @@ int main(void)
 int verifica_collatz(void)
 {
   /* dichiarazione delle variabili locali alla funzione */
-  unsigned long int n;                   /* input: numero naturale */
+  unsigned long int n;     /* input: numero naturale */
   int esito_lettura,       /* lavoro: esito della scanf */
       acquisizione_errata; /* lavoro: esito complessivo dell’acquisizione_errata */
 
   /* acquisire un numero 'n > 0' */
   do
   {
-    printf("inserire un numero magggiore di 0: \n");
+    printf("Inserire un numero 'n' intero > 0: \n");
     esito_lettura = scanf("%lu",
                           &n);
     acquisizione_errata = esito_lettura != 1 || n <= 0 || n >= 4294967295;
     if (acquisizione_errata)
       printf("Valore non accettabile! \n");
-    while (getchar() != '\n');
+    while (getchar() != '\n')
+      ;
   } while (acquisizione_errata);
 
   /* stampare il numero se il suo valore è 1 */
@@ -75,7 +83,8 @@ int verifica_collatz(void)
   /* calcolare il valore del numero fino al raggiungimento del valore 1 */
   else
   {
-    printf("numeri generati:");
+    printf("Numeri generati:\n");
+    printf("----------------\n");
     do
     {
       if (n % 2 == 0)
@@ -87,6 +96,8 @@ int verifica_collatz(void)
       printf("\n%lu",
              n);
     } while (n != 1);
+    printf("\n"); /* vado a capo dalla lista dei numeri generati */
+    printf("Congettura verificata, raggiunto n == 1\n");
   }
   return (0);
 }
@@ -98,8 +109,9 @@ int verifica_primi(unsigned long int numero) /* input:  valore da verificare */
   unsigned long int loop; /* lavoro: controllo ciclo */
   int risultato = 1;      /* output: inizializzo a '1' */
 
+  /* ciclo di verifica se un primo è effettivamente tale */
   for (loop = 2; loop < numero; loop++)
-  { /* ciclo di verifica se un primo è effettivamente tale */
+  {
     if ((numero % loop) == 0)
     {
       risultato = 0;
@@ -107,4 +119,29 @@ int verifica_primi(unsigned long int numero) /* input:  valore da verificare */
   }
 
   return risultato; /*restituisco il risultato della verifica */
+}
+
+/* Definizione della funzione per verificare quanti fattori primi vi sono in comune in una tripla */
+/* di numeri naturali positivi composti >2 */
+int fattori_primi(unsigned long int numero_a,
+                  unsigned long int numero_b,
+                  unsigned long int numero_c)
+{
+  /* dichiarazione delle variabili locali alla funzione */
+  int loop = 2,     /* lavoro: controllo ciclo*/
+      primi_comuni; /* output: numero di numeri primi in comune ad numero_a, numero_b e numero_c */
+
+  while (numero_a >= loop && numero_b >= loop && numero_c >= loop)
+  {
+    if (numero_a % loop == 0 && numero_b % loop == 0 && numero_c % loop == 0)
+    {
+      numero_a /= loop;
+      numero_b /= loop;
+      numero_c /= loop;
+      primi_comuni++;
+    }
+    else
+      loop++;
+  }
+  return (primi_comuni);
 }
