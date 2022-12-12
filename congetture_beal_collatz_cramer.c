70ef5607e71412dd89f114249de623fc0280bab3
l-papadopol <117158814+l-papadopol@users.noreply.github.com>
2022-12-12 21:57:16 +0100

    Add files via upload

--- congetture_beal_collatz_cramer.c
+++ congetture_beal_collatz_cramer.c
@@ -4,7 +4,7 @@
 /* Programma per testare le congetture di Beal, Collatz, Cramèr */
 /*								                                              */
 /* Autori: Papadopol Lucian Ioan 	Matricola: 320648	            */
-/* 	       Spaccamiglio Luca			       329999	                */
+/* 	       Spaccamiglio Luca			           329999             */
 /****************************************************************/
 
 /*****************************/
@@ -18,10 +18,10 @@
 /* Dichiarazione delle funzioni */
 /****************************** */
 
-int verifica_beal(void);
-int verifica_collatz(void);
-
-int verifica_primi(unsigned long int);
+int congettura_beal(void);
+int congettura_collatz(void);
+int congettura_cramer(void);
+int verifica_nprimo(unsigned long int);
 
 /******************************/
 /* Definizione delle funzioni */
@@ -31,9 +31,8 @@ int verifica_primi(unsigned long int);
 int main(void)
 {
   /* dichiarazione delle variabili locali alla funzione */
-  int congettura;          /* input: selezione congettura */
-  int esito_lettura,       /* lavoro: esito della scanf */
-      acquisizione_errata; /* lavoro: esito complessivo dell’acquisizione_errata */
+  int scelta_congettura,        /* input: selezione congettura */
+      esito_acquisizione;       /* lavoro: esito della scanf */
 
   /* acquisizione scelta */
   do
@@ -47,161 +46,177 @@ int main(void)
            "----------------------------------------------------------\n"
            "Digita il numero relativo alla tua scelta e premi invio. \n");
 
-    esito_lettura = scanf("%d",
-                          &congettura);
-    acquisizione_errata = esito_lettura != 1 || congettura > 3 || congettura <= 0;
-    if (acquisizione_errata)
+    /* acquisizione scelta e validazione stretta*/
+    esito_acquisizione = scanf("%d",
+                          &scelta_congettura);
+    esito_acquisizione = esito_acquisizione != 1 || scelta_congettura > 3 || scelta_congettura <= 0;
+    if (esito_acquisizione)
       printf("Valore non accettabile! \n \n");
     while (getchar() != '\n');
-  } while (acquisizione_errata);
+  } while (esito_acquisizione);
 
-  switch (congettura)
+  /* struttura di selezione della congettura*/
+  switch (scelta_congettura)
   {
   case 1:
-    verifica_beal();
+    congettura_beal();
     break;
   case 2:
-    verifica_collatz();
+    congettura_collatz();
+    break;
+  case 3:
+    congettura_cramer();
     break;
   }
   return (0);
 }
 
 /* definizione della funzione per verificare la congettura di Beal */
-int verifica_beal(void)
+int congettura_beal(void)
 {
-  int n[6],
-      esito_lettura,       /* lavoro: esito della scanf */
-      acquisizione_errata, /* lavoro: esito complessivo dell’acquisizione_errata */
+  int parametro_equazione[5],  /* array parametri della equazione*/
+      esito_lettura,          /* lavoro: esito della scanf */
       risultato = 0,
       loop_primi = 2,
       loop_input,
-      lettere[6] = {'a',
-                    'x',
+      lettere[6] = {'a',      /* output: array messaggi per input variabili corrispondenti */
                     'b',
-                    'y',
                     'c',
-                    'z'}; /* Input: array di caratteri */
-  unsigned long int numero_ax,
-                    numero_by,
-                    numero_cz;
-/* acquisire un numero 'n >= 1' per a, b, c */
+                    'x',
+                    'y',
+                    'z'}; 
+  unsigned long int a_esponentex,
+                    b_esponentey,
+                    c_esponentez;
+
+/* Banner esplicativo dei parametri da inserire*/
+printf("------------------------\n"
+      "Equazione a^x + b^y = c^z\n"
+      "-------------------------\n");
+    
+/* acquisire parametri a,b,c,x,y,z e validazione stretta */
   for (loop_input = 0; loop_input < 6; loop_input++)
   { 
-    /* acquisire un numero 'n >= 3' per x, y, z */
     do
     {
-      if (loop_input == 0 || loop_input == 2 || loop_input == 4)
-        printf("Digita un valore %c >= 1: ",
+      if (loop_input <= 2 )
+        printf("Digita valore letterale %c >= 1: ",
                lettere[loop_input]);
       else
-        printf("Digita l'esponente %c >= 3: ",
+        printf("Digita esponente %c >= 3: ",
                lettere[loop_input]);
-
       esito_lettura = scanf("%d",
-                            &n[loop_input]);
-
-      if (loop_input == 0 || loop_input == 2 || loop_input == 4)
-        acquisizione_errata = esito_lettura != 1 || n[loop_input] < 1;
+                            &parametro_equazione[loop_input]);
+      if (loop_input == 0 || loop_input <= 2)
+        esito_lettura = esito_lettura != 1 || parametro_equazione[loop_input] < 1;
       else
-        acquisizione_errata = esito_lettura != 1 || n[loop_input] < 3;
-      if (acquisizione_errata)
+        esito_lettura = esito_lettura != 1 || parametro_equazione[loop_input] < 3;
+      if (esito_lettura)
         printf("Valore non accettabile! \n");
       while (getchar() != '\n');
-    } while (acquisizione_errata);
+    } while (esito_lettura);
   }
 
-  numero_ax = (int)pow(n[0], n[3]);
-  numero_by = (int)pow(n[4], n[1]);
-  numero_cz = (int)pow(n[2], n[5]);
+  /* esegui i calcoli */
+  a_esponentex = pow(parametro_equazione[0], parametro_equazione[3]);
+  b_esponentey = pow(parametro_equazione[1], parametro_equazione[4]);
+  c_esponentez = pow(parametro_equazione[2], parametro_equazione[5]);
 
-  if (numero_ax + numero_by == numero_cz)
+  if (a_esponentex + b_esponentey == c_esponentez)
   {
-    printf("Congettura verificata");
+    printf("Equazione a^x + b^y = c^z verificata");
   }
   else
   {
-    while (numero_ax >= loop_primi && numero_by >= loop_primi && numero_cz >= loop_primi)
+    while (a_esponentex >= loop_primi && b_esponentey >= loop_primi && c_esponentez >= loop_primi)
     {
-      if (numero_ax % loop_primi == 0 && numero_by % loop_primi == 0 && numero_cz % loop_primi == 0)
+      if (a_esponentex % loop_primi == 0 && b_esponentey % loop_primi == 0 && c_esponentez % loop_primi == 0)
       {
-        numero_ax /= loop_primi;
-        numero_by /= loop_primi;
-        numero_cz /= loop_primi;
+        a_esponentex /= loop_primi;
+        b_esponentey /= loop_primi;
+        c_esponentez /= loop_primi;
         risultato++;
       }
       else
         loop_primi++;
     }
-    printf("Congettura non verificata\n"
+
+    /* avviso l'utente dell'esito */
+    printf("Equazione a^x + b^y = c^z non verificata per i parametri inseriti.\n"
            "Vi sono %d numeri primi in comune\n",
            risultato);
   }
   return (0);
 }
 
-/* Definizione della funzione per verificare la congettura di Collatz */
-int verifica_collatz(void)
+/* definizione della funzione per verificare la congettura di Collatz */
+int congettura_collatz(void)
 {
   /* dichiarazione delle variabili locali alla funzione */
-  unsigned long int n;     /* Input: numero naturale */
-  int esito_lettura,       /* lavoro: esito della scanf */
-      acquisizione_errata; /* lavoro: esito complessivo dell’acquisizione_errata */
+  unsigned long int numero_in;        /* input: numero naturale scelto dall'utente*/
+  int esito_lettura;                  /* lavoro: esito della scanf */
 
-  /* acquisire un numero 'n > 0' */
+  /* acquisizione numero da testare e validazione stretta*/
   do
   {
     printf("Inserire un numero 'n' intero > 0: \n");
     esito_lettura = scanf("%lu",
-                          &n);
-    acquisizione_errata = esito_lettura != 1 || n <= 0 || n >= 4294967295;
-    if (acquisizione_errata)
+                          &numero_in);
+    esito_lettura = esito_lettura != 1 || numero_in <= 0;
+    if (esito_lettura)
       printf("Valore non accettabile! \n");
-    while (getchar() != '\n')
-      ;
-  } while (acquisizione_errata);
+    while (getchar() != '\n');
+  } while (esito_lettura);
 
   /* stampare il numero se il suo valore è 1 */
-  if (n == 1)
-    printf("%lu", n);
+  if (numero_in == 1)
+    printf("%lu", numero_in);
 
   /* calcolare il valore del numero fino al raggiungimento del valore 1 */
   else
   {
-    printf("Numeri generati:\n");
-    printf("----------------\n");
+    printf("Numeri generati:\n"     /* Banner evocativo con separatore */
+           "----------------\n");
     do
     {
-      if (n % 2 == 0)
-        n = n / 2;
+      if (numero_in % 2 == 0)
+        numero_in = numero_in / 2;
       else
-        n = (n * 3) + 1;
+        numero_in = (numero_in  * 3) + 1;
 
       /* stampare ogni numero ottenuto dai calcoli */
-      printf("\n%lu",
-             n);
-    } while (n != 1);
+      printf("\n%lu",numero_in);
+    } while (numero_in != 1);
+
+    /* avviso l'utente dell'esito */
     printf("\n"); /* vado a capo dalla lista dei numeri generati */
-    printf("Congettura verificata, raggiunto n == 1\n");
+    printf("Ho raggiunto n == 1\n");
   }
   return (0);
 }
 
+/* Definizione della funzione per verificare la congettura di Collatz */
+int congettura_cramer(void)
+{
+return(0);
+}
+
 /* Definizione della funzione per la verifica se un numero è primo */
-int verifica_primi(unsigned long int numero) /* input:  valore da verificare */
+int verifica_nprimo(unsigned long int numero_in) /* input:  valore da verificare */
 {
   /* dichiarazione delle variabili locali alla funzione */
-  unsigned long int loop; /* lavoro: controllo ciclo */
-  int risultato = 1;      /* output: inizializzo a '1' */
+  unsigned long int i;    /* lavoro: controllo ciclo */
+  int risultato = 1;      /* output: variabile risultato della verifica inizializzo a '1' ovvero 'è primo' */
 
   /* ciclo di verifica se un primo è effettivamente tale */
-  for (loop = 2; loop < numero; loop++)
+  for (i = 2; i < numero_in; i++)
   {
-    if ((numero % loop) == 0)
+    if ((numero_in % i) == 0)
     {
-      risultato = 0;
+      risultato = 0;  /* non è primo*/
     }
   }
 
-  return risultato; /* restituisco il risultato della verifica */
+  /* restituisco il risultato della verifica */
+  return risultato; 
 }
